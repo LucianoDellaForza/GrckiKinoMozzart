2d0b5ba2d7c891196bd0d8f28508d99cdb34cc63
Luka Krivacevic <luka.krivacevic@cinnamon.agency>
2024-09-09 18:22:27 +0200

    WebView fix

--- app/src/main/java/com/luka/grcki_kino_mozzart/ui/feature_main/draw_live/LiveDrawScreen.kt
+++ app/src/main/java/com/luka/grcki_kino_mozzart/ui/feature_main/draw_live/LiveDrawScreen.kt
@@ -2,12 +2,9 @@ package com.luka.grcki_kino_mozzart.ui.feature_main.draw_live
 
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 
@@ -18,24 +15,19 @@ fun LiveDrawScreen(
 ) {
     val url = remember { "https://oldsite.mozzartbet.com/sr/lotto-animation/26/#/" }
 
-    Column(
-        modifier = modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        AndroidView(
-            modifier = Modifier.fillMaxSize(),
-            factory = { context ->
-                WebView(context).apply {
-                    webViewClient = WebViewClient()
-                    settings.javaScriptEnabled = true
-                    loadUrl(url)
-                }
-            },
-            update = { webView ->
-                webView.loadUrl(url)
+    AndroidView(
+        modifier = modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+
+                webViewClient = WebViewClient()
+                loadUrl(url)
             }
-        )
-    }
+        },
+        update = { webView ->
+            webView.loadUrl(url)
+        }
+    )
 }
